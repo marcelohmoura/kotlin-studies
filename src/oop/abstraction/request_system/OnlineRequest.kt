3b12b2f36b214505c4b2a8cef77c009b823fc1a6
Marcelo Moura <marcelohmoura99@outlook.com.br>
2024-12-27 14:16:56 -0300

    refactoring

--- src/oop/abstraction/request_system/OnlineRequest.kt
+++ src/oop/abstraction/request_system/OnlineRequest.kt
@@ -1,11 +1,6 @@
 package oop.abstraction.request_system
 
 class OnlineRequest : Requests() {
-    override fun totalCalculate(items : List<Request>) : Double {
-        return items.sumOf { it.value }
-    }
-
-    override fun showDetails(items : List<Request>) : List<Map<String, Any>> {
-        return items.map { item -> mapOf(item.productName to item.value) }
-    }
+    override fun totalCalculate(items : List<Request>) = items.sumOf { it.productPrice }
+    override fun showDetails(items : List<Request>) = items.map { item -> mapOf(item.productName to item.productPrice) }
 }
--- src/oop/abstraction/request_system/Request.kt
+++ src/oop/abstraction/request_system/Request.kt
@@ -1,3 +1,3 @@
 package oop.abstraction.request_system
 
-class Request(val productName: String, val value: Double)
+class Request(val productName: String, val productPrice: Double, val sellerId: String = "online-request")
